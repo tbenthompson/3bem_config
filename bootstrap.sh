59109b7da95d6075ac0331b33c5aef968de53341
Ben Thompson <t.ben.thompson@gmail.com>
2015-01-22 14:34:16 -0500

    Working towards a bootstrap process.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# GITPROTOCOL="https://"
+GITPROTOCOL="git@"
 # Install Docker
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
@@ -6,20 +8,16 @@ curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 sudo apt-get install git
 
 # Download the environment configuration
-git clone https://github.com/tbenthompson/3bem_env.git
+git clone $(GITPROTOCOL)github.com/tbenthompson/3bem_env.git
 # Build the environment
 cd 3bem_env;
 ./build;
 
 # Download the libraries and tools and build them
 cd ../;
-git clone https://github.com/tbenthompson/3bem.git 3bem_dev
-git clone -b stable --single-branch https://github.com/tbenthompson/3bem.git 3bem_stable
-git clone https://github.com/tbenthompson/elastic.git
+git clone $(GITPROTOCOL)github.com/tbenthompson/3bem.git 3bem_dev
+git clone -b stable --single-branch \
+    $(GITPROTOCOL)github.com/tbenthompson/3bem.git 3bem_stable
+git clone $(GITPROTOCOL)github.com/tbenthompson/elastic.git
 
-./start "
-cd 3bem_stable; 
-python build.py;
-cd ../elastic;
-python build.py;
-"
+./3bem_env/start "cd /home/3bem/3bem_stable; python build.py; cd /home/3bem/elastic; python build.py;"
