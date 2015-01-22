2b676ee29308f06ba92f6f384ef32c32af0f9fc5
Ben Thompson <t.ben.thompson@gmail.com>
2015-01-22 14:53:38 -0500

    Fixed environment variable problem with PETSC_DIR/ARCH.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# GITPROTOCOL="https://"
-GITPROTOCOL="git@"
+# GITPROTOCOL="https://github.com/"
+GITPROTOCOL=git@github.com:
+
 # Install Docker
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
@@ -8,16 +9,17 @@ curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 sudo apt-get install git
 
 # Download the environment configuration
-git clone $(GITPROTOCOL)github.com/tbenthompson/3bem_env.git
+git clone ${GITPROTOCOL}tbenthompson/3bem_env.git
+git@github.com:tbenthompson/3bem_env.git
 # Build the environment
 cd 3bem_env;
 ./build;
 
 # Download the libraries and tools and build them
 cd ../;
-git clone $(GITPROTOCOL)github.com/tbenthompson/3bem.git 3bem_dev
+git clone ${GITPROTOCOL}tbenthompson/3bem.git 3bem_dev
 git clone -b stable --single-branch \
-    $(GITPROTOCOL)github.com/tbenthompson/3bem.git 3bem_stable
-git clone $(GITPROTOCOL)github.com/tbenthompson/elastic.git
+    ${GITPROTOCOL}tbenthompson/3bem.git 3bem_stable
+git clone ${GITPROTOCOL}tbenthompson/elastic.git
 
-./3bem_env/start "cd /home/3bem/3bem_stable; python build.py; cd /home/3bem/elastic; python build.py;"
+./3bem_env/start "cd /home/3bem/3bem_stable; PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH python build.py; cd /home/3bem/elastic; PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH python build.py;"
